8f565141fe446592f8d9788503d03edf710d16db
Rafaela Bertuol <rafabertuol@gmail.com>
2024-06-22 13:36:30 +0000

    Added first version of staging and dimensions tabela for the project. Deleted examples files.

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,8 +1,9 @@
 with    
     dim_customers as (
-        select *
+        select
+            {{dbt_utils.generate_surrogate_key(['customer_id', 'company_name'])}} as sk_customer
+            , *
         from {{ref("stg_customers")}}
     )
-
     select *
     from dim_customers
